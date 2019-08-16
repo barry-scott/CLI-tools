4aa6a25fc1f2284a535d345b3cee4a553eeb9eed
Barry Scott <barry@barrys-emacs.org>
2019-08-16 10:47:37 +0100

    Need print_function for python 2 support.

--- Source/ssh_wait/__init__.py
+++ Source/ssh_wait/__init__.py
@@ -1,7 +1,8 @@
+from __future__ import print_function
 import time
 import socket
 
-VERSION='1.0.0'
+VERSION='1.0.1'
 
 def ssh_wait( host, service='ssh', wait=True, wait_limit=600, log_fn=print ):
     try:
