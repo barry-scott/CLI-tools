349fca83028b397b29d2842c7f6f4ab3fcd4f11e
Barry Scott <barry@barrys-emacs.org>
2019-08-16 10:41:27 +0100

    Build universal wheels as all the tools can run on python 2.7 or python 3.

--- Source/build.sh
+++ Source/build.sh
@@ -10,24 +10,19 @@ function build_tool {
     ${PY} -m colour_text "<>green Info:<> building <>yellow %s<>" ${1}
     # setup tools insists on finding the README.md in the current folder
     cp -f ${1}/README.md .
-    ${PY} setup_${1}.py --quiet sdist bdist_wheel
+    ${PY} setup_${1}.py --quiet sdist bdist_wheel "${2}"
     rm -f README.md
 }
 
 if [[ "$1" = "" ]]
 then
-    for tool in \
-        colour_text \
-        colour_filter \
-        ssh_wait \
-        smart_find \
-        ;
-    do
-        build_tool "${tool}"
-    done
+    build_tool colour_text --universal
+    build_tool colour_filter --universal
+    build_tool ssh_wait --universal
+    build_tool smart_find --universal
 
 else
-    build_tool "${1}"
+    build_tool "${1}" "${2}"
 fi
 
 
