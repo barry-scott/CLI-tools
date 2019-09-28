07ac55623f95875d9c6be71aba4fdc95994c93c3
Barry Scott <barry@barrys-emacs.org>
2019-09-28 17:43:43 +0100

    pip checks the current folder for packages so must not install from the Source folder.

--- Source/localinstall.sh
+++ Source/localinstall.sh
@@ -2,5 +2,9 @@
 for PY in python3.7 python2.7
 do
     ${PY} -m colour_text "<>green Info:<> Installing wheels for ${PY}"
-    ${PY} -m pip install --upgrade --user dist/*.whl
+    d=$(pwd)
+    pushd ${TMPDIR}
+    # cannot install from the source dir
+    ${PY} -m pip install --upgrade --user --no-warn-script-location ${d}/dist/*.whl
+    popd
 done
