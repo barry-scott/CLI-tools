4e6b6d475dd9b4bb968c705a7418546febfe3599
Barry Scott <barry@barrys-emacs.org>
2023-07-31 17:17:49 +0100

    update upload with new dist locations

--- upload-to-pypi.sh
+++ upload-to-pypi.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -e
 export TWINE_USERNAME=barryscott
+PACKAGE=${1:?package to upload}
+if [ ! -d "$PACKAGE" ]
+then
+    echo "Error: unknown package ${PACKAGE}"
+    exit 1
+fi
 
-tmp.venv/bin/python -m twine check dist/*
-tmp.venv/bin/python -m twine upload dist/*
+tmp.venv/bin/python -m twine check ${PACKAGE}/dist/*
+tmp.venv/bin/python -m twine upload ${PACKAGE}/dist/*
