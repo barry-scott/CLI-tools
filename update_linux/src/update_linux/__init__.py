481a2489881c73ed7e2d07bf8d01cf7f652edf96
Barry Scott <barry@barrys-emacs.org>
2024-04-27 20:41:05 +0100

    Update for issues seen upgrading to f40 when run from macOS.

--- update_linux/src/update_linux/__init__.py
+++ update_linux/src/update_linux/__init__.py
@@ -9,7 +9,7 @@ import tempfile
 import json
 from config_path import ConfigPath  # type: ignore
 
-VERSION = '3.1.0'
+VERSION = '3.1.1'
 
 default_json_config_template = u'''{
     "group":
@@ -418,11 +418,22 @@ For help:
     def reboot( self, host, cmd, wait_limit=600 ):
         while True:
             rc, stdout = self.runAndLog( host, cmd )
-            if len(stdout) == 0:
+            # filter out warning/info messages before checking for success
+            filtered_stdout = []
+            for line in stdout:
+                if line.startswith(
+                        ('Warning: '
+                        ,'Offending key for '
+                        ,'Matching host key in ')):
+                    continue
+                filtered_stdout.append(line)
+
+            if len(filtered_stdout) == 0:
                 # no messages assume ok
                 break
 
-            if stdout[0] == ('Connection to %s closed by remote host.\r\n' % (host,)):
+            if( filtered_stdout[0].startswith('Connection to ')
+            and filtered_stdout[0].endswith('closed by remote host.\r\n') ):
                 # this is success
                 break
 
@@ -662,7 +673,15 @@ class UpdatePluginFedora:
 
     def releaseInfo( self, host ):
         cmd = ['cat', '/etc/system-release-cpe']
-        rc, stdout = self.app.runAndLog( host, cmd, log=False )
+        # first boot after a system upgrade seen to block this access
+        log = False
+        for _ in range(10):
+            rc, stdout = self.app.runAndLog( host, cmd, log=log )
+            if 'System is booting up' not in stdout[0]:
+                break
+            log = True
+            time.sleep(5)
+
         cpe_parts = stdout[0].strip().split( ':' )
         if len(cpe_parts) >= 4:
             return int( cpe_parts[4] )
